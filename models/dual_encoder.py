b646f5dd5e4de8cd39655625bf18e0e36a1f1543
Wouter Oosterheert <wouter@Wouters-Mac-mini.local>
2025-01-28 14:46:05 +0100

    udpates dual encoder

--- models/dual_encoder.py
+++ models/dual_encoder.py
@@ -3,26 +3,31 @@ import torch
 from torch import optim
 import pytorch_lightning as pl
 
-class rsvqa_pl(pl.LightningModule):
-    def __init__(self, vision_encoder, text_encoder):
+class dual_encoder_with_classifier(pl.LightningModule):
+    def __init__(self, vision_encoder, text_encoder, vision_encoder_dim, text_encoder_dim, use_vision_cls_token=False):
         super().__init__()
 
         self.vision_encoder = vision_encoder
         self.text_encoder = text_encoder
+        self.vision_encoder_dim = vision_encoder_dim
+        self.text_encoder_dim = text_encoder_dim
+        self.use_vision_cls_token = use_vision_cls_token
+
         for param in self.vision_encoder.parameters():
             param.requires_grad = False
         for param in self.text_encoder.parameters():
             param.requires_grad = False
 
-        self.fusion_layer = nn.Sequential(nn.Linear(768+768, 128), nn.ReLU())
+        self.fusion_layer = nn.Sequential(nn.Linear(self.vision_encoder_dim+self.text_encoder_dim, 128), nn.ReLU())
         self.classification_layer = nn.Sequential(nn.Linear(128, 1), nn.Sigmoid())
 
     def forward(self, normalised_image, text_tokens, attention_mask):
         img_embedding, _, _ = self.vision_encoder(normalised_image)
-        img_cls = img_embedding[:,0,:]
+        if self.use_vision_cls_token:
+            img_embedding = img_embedding[:,0,:]
         txt_embedding = self.text_encoder(input_ids=text_tokens, attention_mask=attention_mask)
-        txt_cls = txt_embedding.last_hidden_state[:,0,:]
-        fused_embedding = torch.cat([img_cls, txt_cls], dim=1)
+        txt_embedding = txt_embedding.last_hidden_state[:,0,:]
+        fused_embedding = torch.cat([img_embedding, txt_embedding], dim=1)
         fused_projection = self.fusion_layer(fused_embedding)
         prediction = self.classification_layer(fused_projection)
         return prediction
