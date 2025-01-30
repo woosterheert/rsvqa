f154d70f4e9d6e8c78cf9ab066126eb6860a8ce6
Wouter Oosterheert <wouter@Wouters-Mac-mini.local>
2025-01-30 13:30:30 +0100

    adds training

--- models/prithvi_bert_training.py
+++ models/prithvi_bert_training.py
@@ -32,8 +32,8 @@ bert_encoder = BertModel.from_pretrained('bert-base-uncased')
 train_ds = RSVQADataset('/home/wouter/data/prithvi_bert_small')
 val_ds = RSVQADataset('/home/wouter/data/prithvi_bert_small_validation')
 
-train_dataloader = DataLoader(train_ds, batch_size=8, shuffle=True, num_workers=4, pin_memory=True)
-val_dataloader = DataLoader(val_ds, batch_size=8, shuffle=False, num_workers=4, pin_memory=True)
+train_dataloader = DataLoader(train_ds, batch_size=4, shuffle=True, num_workers=4, pin_memory=True)
+val_dataloader = DataLoader(val_ds, batch_size=4, shuffle=False, num_workers=4, pin_memory=True)
 
 baseline_model_with_prithvi = dual_encoder_with_classifier(prithvi_encoder, bert_encoder, 768, 768, "6d")
 
--- utils/dual_encoder_dataset.py
+++ utils/dual_encoder_dataset.py
@@ -1,12 +1,17 @@
 from torch.utils.data import Dataset
 import torch
 import os
+import random
 
 class RSVQADataset(Dataset):
-    def __init__(self, data_dir):
+    def __init__(self, data_dir, frac=1.0):
         super().__init__()
         self.data_dir = data_dir
         self.file_names = [x for x in os.listdir(self.data_dir) if x.endswith('.pt')]
+        self.frac = frac
+        if self.frac < 1:
+            nr = int(self.frac * len(self.file_names))
+            self.file_names = random.sample(self.file_names, nr)
 
     def __len__(self):
         return len(self.file_names)
