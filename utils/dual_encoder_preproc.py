3f1f2c4a6f0daed61ca67d3a7183b0e1134d6f05
Wouter Oosterheert <wouter@Wouters-Mac-mini.local>
2025-01-31 11:11:57 +0100

    upates preproc

--- utils/dual_encoder_preproc.py
+++ utils/dual_encoder_preproc.py
@@ -65,7 +65,7 @@ class RSVQADataProcessor:
 
 if __name__ == "__main__" :
     
-    df = pd.read_csv('data/questions_and_answers_binary.csv', index_col=0)
+    df = pd.read_csv('/home/wouter/data/questions_and_answers_binary.csv', index_col=0)
     
     df_train = df.query("split == 'train'")
     df_train_pos = df_train.query('binary_answer==1').sample(5000)  
@@ -78,15 +78,15 @@ if __name__ == "__main__" :
     df_val_balanced = pd.concat([df_val_pos, df_val_neg]).sample(frac=1)
 
     tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
-    with open("config.yaml") as f:
+    with open("/home/wouter/daa/config.yaml") as f:
         model_config = yaml.safe_load(f)
     train_args = model_config["train_params"]
 
-    train_proc = RSVQADataProcessor(df_train_balanced, train_args, 'data/rgb_data', 'data/resnet_bert/training', tokenizer, "rgb")
+    train_proc = RSVQADataProcessor(df_train_balanced, train_args, '/home/wouter/data/rgb_data', '/home/wouter/data/resnet_bert/training', tokenizer, "rgb")
     train_proc.process()
-    train_proc = RSVQADataProcessor(df_train_balanced, train_args, 'data/6d_data', 'data/prithvi_bert/training', tokenizer, "6d")
+    train_proc = RSVQADataProcessor(df_train_balanced, train_args, '/home/wouter/data/6d_data', '/home/wouter/data/prithvi_bert/training', tokenizer, "6d")
     train_proc.process()
-    train_proc = RSVQADataProcessor(df_val_balanced, train_args, 'data/rgb_data', 'data/resnet_bert/validation', tokenizer, "rgb")
+    train_proc = RSVQADataProcessor(df_val_balanced, train_args, '/home/wouter/data/rgb_data', '/home/wouter/data/resnet_bert/validation', tokenizer, "rgb")
     train_proc.process()
-    train_proc = RSVQADataProcessor(df_val_balanced, train_args, 'data/6d_data', 'data/prithvi_bert/validation', tokenizer, "6d")
+    train_proc = RSVQADataProcessor(df_val_balanced, train_args, '/home/wouter/data/6d_data', '/home/wouter/data/prithvi_bert/validation', tokenizer, "6d")
     train_proc.process()
