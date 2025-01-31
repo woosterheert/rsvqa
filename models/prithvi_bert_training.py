aad8594bc70ca04bea3acd7fcbedeaed72998366
Wouter Oosterheert <wouter@Wouters-Mac-mini.local>
2025-01-31 16:20:09 +0100

    updates training

--- models/prithvi_bert_training.py
+++ models/prithvi_bert_training.py
@@ -9,7 +9,7 @@ from models.load_models import load_prithvi_bert
 prithvi_bert_classifier = load_prithvi_bert()
 
 train_ds = RSVQADataset('/home/wouter/data/prithvi_bert/training')
-val_ds = RSVQADataset('/home/wouter/data/prithvi_bert/validation', frac=0.1)
+val_ds = RSVQADataset('/home/wouter/data/prithvi_bert/validation')
 train_dataloader = DataLoader(train_ds, batch_size=16, shuffle=True, num_workers=4, pin_memory=True)
 val_dataloader = DataLoader(val_ds, batch_size=16, shuffle=False, num_workers=4, pin_memory=True)
 
--- models/resnet_bert_training.py
+++ models/resnet_bert_training.py
@@ -9,7 +9,7 @@ from models.load_models import load_resnet_bert
 resnet_bert_classifier = load_resnet_bert()
 
 train_ds = RSVQADataset('/home/wouter/data/resnet_bert/training')
-val_ds = RSVQADataset('/home/wouter/data/resnet_bert/validation', frac=0.1)
+val_ds = RSVQADataset('/home/wouter/data/resnet_bert/validation')
 train_dataloader = DataLoader(train_ds, batch_size=16, shuffle=True, num_workers=4, pin_memory=True)
 val_dataloader = DataLoader(val_ds, batch_size=16, shuffle=False, num_workers=4, pin_memory=True)
 
