436c0ed541cae5275fca05bbad7b6f209d838b83
Wouter Oosterheert <wouter@Wouters-Mac-mini.local>
2025-02-01 08:42:37 +0100

    updates training

--- models/prithvi_bert_training.py
+++ models/prithvi_bert_training.py
@@ -13,5 +13,5 @@ val_ds = RSVQADataset('/home/wouter/data/prithvi_bert/validation')
 train_dataloader = DataLoader(train_ds, batch_size=16, shuffle=True, num_workers=4, pin_memory=True)
 val_dataloader = DataLoader(val_ds, batch_size=16, shuffle=False, num_workers=4, pin_memory=True)
 
-trainer = pl.Trainer(max_epochs=5)
+trainer = pl.Trainer(max_epochs=10)
 trainer.fit(model=prithvi_bert_classifier, train_dataloaders=train_dataloader, val_dataloaders=val_dataloader)
