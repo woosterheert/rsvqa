f1e7afbe77879a8f6b09220bc5577085fa77e224
Wouter Oosterheert <wouter@Wouters-Mac-mini.local>
2025-02-05 13:32:39 +0100

    run models for app

--- models/run_models.py
+++ models/run_models.py
@@ -1,20 +1,57 @@
 import sys
 sys.path.insert(1, '/home/wouter/rsvqa')
 
-from transformers import BertModel
-from rsvqa.models.dual_encoder import dual_encoder_with_classifier
-from torchvision.models import resnet50
-import pytorch_lightning as pl
-from rsvqa.external.prithvi_mae import PrithviViT
-import torch
-import yaml
 import pandas as pd
-from rsvqa.utils.dual_encoder_preproc import RSVQADataProcessor
-from rsvqa.utils.paligemma_preproc import create_dataset
-from transformers import BertTokenizer
+from models.load_models import load_prithvi_bert, load_resnet_bert
+from datasets import load_from_disk
+import os
+import torch
+
+def load_data(file_path):
+    data = torch.load(file_path)
+    image = data['image']
+    input_ids = data['input_ids']
+    attention_mask = data['attention_mask']
+    label = data['label']
+    return image, input_ids, attention_mask, label
+
+if __name__ == "__main__" :
+
+    resnet_bert_ckpt = "/home/wouter/rsvqa/lightning_logs/version_11/checkpoints/'epoch=9-step=6250.ckpt'"
+    prithvi_bert_ckpt = "/home/wouter/rsvqa/lightning_logs/version_12/checkpoints/'epoch=9-step=6250.ckpt'"
+    resnet_bert_model = load_resnet_bert(resnet_bert_ckpt)
+    resnet_bert_model.eval()
+    prithvi_bert_model = load_prithvi_bert(prithvi_bert_ckpt)
+    prithvi_bert_model.eval()
+
+    df = pd.read_csv('/home/wouter/data/app/df.csv').rename(columns={'Unnamed: 0': 'idx'})
+    paligemma_data = load_from_disk('/home/wouter/data/app/paligemma')
+
+    resnet_predictions = []
+    prithvi_predictions = []
+
+    for i in range(20):
+        resnet_data_path = os.path.join('/home/wouter/data/app/resnet_bert', df.iloc[i].idx) 
+        image, input_ids, attention_mask, label = load_data(resnet_data_path)
+
+        with torch.no_grad():
+            resnet_predictions.append(resnet_bert_model(image, input_ids, attention_mask))
+
+        prithvi_data_path = os.path.join('/home/wouter/data/app/prithvi_bert', df.iloc[i].idx) 
+        image, input_ids, attention_mask, label = load_data(prithvi_data_path)
+        
+        with torch.no_grad():
+            prithvi_predictions.append(prithvi_bert_model(image, input_ids, attention_mask))
+        
+    df['resnet_prediction'] = resnet_predictions
+    df['prithvi_prediction'] = prithvi_predictions
+
+    df.to_csv('/home/wouter/data/app/df_results.csv')
 
 
-resnet_bert_ckpt = "/home/wouter/rsvqa/lightning_logs/version_11/checkpoints/'epoch=9-step=6250.ckpt'"
-prithvi_bert_ckpt = "/home/wouter/rsvqa/lightning_logs/version_12/checkpoints/'epoch=9-step=6250.ckpt'"
-resnet_bert_model = load_resnet_bert(resnet_bert_ckpt)
-prithvi_bert_model = load_prithvi_bert(prithvi_bert_ckpt)
+
+
+
+
+
+
