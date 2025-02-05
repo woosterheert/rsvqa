e1d3530dbdf65a69e0100a307c571a8ebbde411c
Wouter Oosterheert <wouter@Wouters-Mac-mini.local>
2025-02-05 11:36:49 +0100

    updates preproc

--- utils/paligemma_preproc.py
+++ utils/paligemma_preproc.py
@@ -19,20 +19,22 @@ def create_dataset(df, data_dir):
     dataset = Dataset.from_list(data)
     return dataset
 
-df = pd.read_csv('/home/wouter/data/questions_and_answers_binary_new.csv', index_col=0)
+if __name__ == "__main__" :
 
-df_train = df.query("split == 'train'")
-df_train_pos = df_train.query('binary_answer==1').sample(5000)  
-df_train_neg = df_train.query('binary_answer==0').sample(5000)
-df_train_balanced = pd.concat([df_train_pos, df_train_neg]).sample(frac=1)
+    df = pd.read_csv('/home/wouter/data/questions_and_answers_binary_new.csv', index_col=0)
 
-df_val = df.query("split == 'validation'")
-df_val_pos = df_val.query('binary_answer==1').sample(100)  
-df_val_neg = df_val.query('binary_answer==0').sample(100)
-df_val_balanced = pd.concat([df_val_pos, df_val_neg]).sample(frac=1)
+    df_train = df.query("split == 'train'")
+    df_train_pos = df_train.query('binary_answer==1').sample(5000)  
+    df_train_neg = df_train.query('binary_answer==0').sample(5000)
+    df_train_balanced = pd.concat([df_train_pos, df_train_neg]).sample(frac=1)
 
-train_ds = create_dataset(df_train_balanced, '/home/wouter/data/rgb_data')
-val_ds = create_dataset(df_val_balanced, '/home/wouter/data/rgb_data')
+    df_val = df.query("split == 'validation'")
+    df_val_pos = df_val.query('binary_answer==1').sample(100)  
+    df_val_neg = df_val.query('binary_answer==0').sample(100)
+    df_val_balanced = pd.concat([df_val_pos, df_val_neg]).sample(frac=1)
 
-train_ds.save_to_disk('/home/wouter/data/paligemma_train')
-val_ds.save_to_disk('/home/wouter/data/paligemma_val')
+    train_ds = create_dataset(df_train_balanced, '/home/wouter/data/rgb_data')
+    val_ds = create_dataset(df_val_balanced, '/home/wouter/data/rgb_data')
+
+    train_ds.save_to_disk('/home/wouter/data/paligemma_train')
+    val_ds.save_to_disk('/home/wouter/data/paligemma_val')
